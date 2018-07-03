d7451c532ad8e1e9aecf2d3b6c1ecde8f58355a0
daouda.diallo <daouda.diallo@sfl.ci>
2018-07-03 10:46:20 +0000

    ajout list endpoint

--- server.js
+++ server.js
@@ -2,4 +2,6 @@ var app = require('./app');
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
     console.log('Express server listening on port ' + port);
+    const all_routes = require('express-list-endpoints');
+    console.log(all_routes(app));
 });
